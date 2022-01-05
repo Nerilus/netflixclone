6102681a5573a04cf7bbedc1da19eb58a96ba5b9
User <user@MacBook-Pro-Sosa.local>
2022-01-05 14:06:46 +0100

    first commit

---
+++ src/components/jumbotron/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { SubTitle, Title, Pane, Container, Inner, Image } from './styles/jumbotron';
+
+export default function Jumbotron({children, direction = 'row', ...restProps}){
+    return (
+        <Inner>
+            {children}
+        </Inner>
+    )
+}
+
+Jumbotron.Container = function JumboContainer({ children, ...restProps }) {
+    return <Container {...restProps}>{children}</Container>;
+}
+
+Jumbotron.Pane = function JumboPane({ children, ...restProps }) {
+    return <Pane {...restProps}>{children}</Pane>;
+}
+Jumbotron.Title = function JumboTitle({ children, ...restProps }) {
+    return <Title {...restProps}>{children}</Title>;
+}
+Jumbotron.SubTitle = function JumboSubTitle({ children, ...restProps }) {
+    return <SubTitle {...restProps}>{children}</SubTitle>;
+}
+Jumbotron.Image = function JumboImagee({...restProps }) {
+    return <Image {...restProps}/>;
+}
